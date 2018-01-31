f3e3eadf804f68bddc31f5177281d3de6a7fd5b6
Kevin Mougammadaly <kevin.mougammadaly@gmail.com>
2018-01-31 22:13:15 +0100

    Updating config fosrest

--- site/src/AppBundle/Controller/APIController.php
+++ site/src/AppBundle/Controller/APIController.php
@@ -9,30 +9,36 @@
 namespace AppBundle\Controller;
 
 
-use FOS\RestBundle\FOSRestBundle;
-use FOS\RestBundle\Controller\Annotations\Route;
-
-
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\User;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\View;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+use FOS\RestBundle\Controller\Annotations as Rest; // alias pour toutes les annotations
 
 
-class APIController extends FOSRestBundle
+class APIController extends Controller
 {
     /**
-     * @Route("/user", name="user", methods={"GET"})
-     * @param Request $request
+     * @Rest\View()
+     * @Rest\Post("/user")
      */
-    public function testAction(Request $request)
+    public function getUserAction(Request $request)
     {
+        $login = $request->request->get('login');
+        $password = $request->request->get('password');
 
-        $em = $this->container->get('doctrine.orm.entity_manager');
-        $userRepository = $em->getRepository('AppBundle:User');
-        $data =  $this->container->get('serializer')->serialize($userRepository->find(1), 'json');
 
-        return new JsonResponse(array('object' => $data ));
+        $logger = $this->container->get('logger');
+        $places = $this->container->get('doctrine.orm.entity_manager')
+            ->getRepository('AppBundle:User')
+            ->findByLogin($login);
+
+        $logger->info($request->request->get($places));
+        return $places;
     }
 }
